652ed3ca386983a84f9816ad4ab0b72c3f73f7c7
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-02-06 16:31:35 +0530

    Add base screens

---
+++ screens/CompletedScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ScrollView, Text, StyleSheet } from 'react-native';
+import { Container, Header, View, Button, Icon, Fab } from 'native-base';
+
+import { ExpoLinksView } from '@expo/samples';
+import AddTodoButton from '../components/AddTodoButton';
+
+export default class CompletedScreen extends React.Component {
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <AddTodoButton />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+});
