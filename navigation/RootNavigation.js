9c30d050da4eed49eed6a805862472a7effb36ba
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-02-07 18:18:21 +0530

    Load native-base fonts, show spinner

--- navigation/RootNavigation.js
+++ navigation/RootNavigation.js
@@ -1,10 +1,12 @@
-import { Notifications } from 'expo';
+import Expo, { Notifications } from 'expo';
 import React from 'react';
+import { StatusBar, View } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-import { Container } from 'native-base';
+import { Container, Spinner } from 'native-base';
 import { ReactiveBase } from '@appbaseio/reactivesearch-native';
 
 import CONFIG from '../constants/Config';
+import COLORS from '../constants/Colors';
 import MainTabNavigator from './MainTabNavigator';
 import registerForPushNotificationsAsync from '../api/registerForPushNotificationsAsync';
 
@@ -24,21 +26,25 @@ const RootStackNavigator = StackNavigator(
 );
 
 export default class RootNavigator extends React.Component {
+  state = {
+    isReady: false,
+  };
+
   componentDidMount() {
     this._notificationSubscription = this._registerForPushNotifications();
   }
 
-  componentWillUnmount() {
+  async componentWillMount() {
     this._notificationSubscription && this._notificationSubscription.remove();
-  }
 
-  render = () => (
-    <ReactiveBase app={CONFIG.app} credentials={CONFIG.credentials} type={CONFIG.type}>
-      <Container>
-        <RootStackNavigator />
-      </Container>
-    </ReactiveBase>
-  );
+    await Expo.Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      Ionicons: require('native-base/Fonts/Ionicons.ttf'),
+    });
+
+    this.setState({ isReady: true });
+  }
 
   _registerForPushNotifications() {
     // Send our push token over to our backend so we can receive notifications
@@ -53,5 +59,26 @@ export default class RootNavigator extends React.Component {
 
   _handleNotification = ({ origin, data }) => {
     console.log(`Push notification ${origin} with data: ${JSON.stringify(data)}`);
+  };
+
+  renderStatusBar = () => <StatusBar backgroundColor={COLORS.secondary} barStyle="dark-content" />;
+
+  render = () => {
+    if (!this.state.isReady) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          {this.renderStatusBar()}
+          <Spinner color={COLORS.primary} />
+        </View>
+      );
+    }
+
+    return (
+      <ReactiveBase app={CONFIG.app} credentials={CONFIG.credentials} type={CONFIG.type}>
+        <Container>
+          <RootStackNavigator />
+        </Container>
+      </ReactiveBase>
+    );
   };
 }
