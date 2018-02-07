83d0707a1a4f38c19a08f3f86352c048124f2869
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-02-07 17:27:47 +0530

    Setup eslint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: 'airbnb',
+  rules: {
+    indent: 0,
+
+    'react/jsx-indent': 0,
+    'react/jsx-indent-props': 0,
+    'react/jsx-filename-extension': 0,
+    'import/no-extraneous-dependencies': 0,
+  },
+  parser: 'babel-eslint',
+};
