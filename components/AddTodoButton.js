f644445dc42996dc652493d599ea146f5f7e806e
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-02-09 18:17:03 +0530

    Add component AddTodo fab button

--- components/AddTodoButton.js
+++ components/AddTodoButton.js
@@ -1,21 +1,40 @@
+/* eslint-disable */
 import React, { Component } from 'react';
-import { Container, Header, View, Button, Icon, Fab } from 'native-base';
+import PropTypes from 'prop-types';
+import { Icon, View, Fab } from 'native-base';
+import { FAB } from 'react-native-paper';
+import COLORS from '../constants/Colors';
+/* eslint-enable */
 
-export default class AddTodoButton extends Component {
-  state = {
-    active: false,
-  };
+const propTypes = {
+  onPress: PropTypes.func.isRequired,
+};
 
-  render = () => (
-    <Fab
-      active={this.state.active}
-      direction="up"
-      containerStyle={{}}
-      style={{ backgroundColor: '#5067FF' }}
-      position="bottomRight"
-      onPress={() => this.setState({ active: !this.state.active })}
-    >
-      <Icon name="add" />
-    </Fab>
-  );
-}
+const AddTodoButton = ({ onPress }) => (
+  <View
+    style={{
+      position: 'absolute',
+      bottom: 0,
+      right: 0,
+      padding: 25,
+    }}
+  >
+    <FAB icon="add" color="white" style={{ backgroundColor: COLORS.primary }} onPress={onPress} />
+  </View>
+);
+
+// const AddTodoButton = ({ onPress }) => (
+//   <Fab
+//     direction="up"
+//     containerStyle={{}}
+//     style={{ backgroundColor: '#5067FF' }}
+//     position="bottomRight"
+//     onPress={onPress}
+//   >
+//     <Icon name="add" />
+//   </Fab>
+// );
+
+AddTodoButton.propTypes = propTypes;
+
+export default AddTodoButton;
