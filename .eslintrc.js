1a82ea4ce923e6cb2ed66fb8f26bb88f4b3a1742
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-02-09 18:16:14 +0530

    Setup eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,11 @@ module.exports = {
     'react/jsx-indent-props': 0,
     'react/jsx-filename-extension': 0,
     'import/no-extraneous-dependencies': 0,
+    'no-underscore-dangle': 0,
+    'no-console': 0,
+    'no-plusplus': 0,
+    'default-case': 0,
+    'global-require': 0,
   },
   parser: 'babel-eslint',
 };
